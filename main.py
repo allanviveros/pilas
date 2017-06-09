8e5291e30a57f5b8cdd621968f1f3a71cb9bd27c
allan <allan.viveros.14@sansano.usm.cl>
2017-06-08 23:48:17 -0400

    <begin3>:

--- main.py
+++ main.py
@@ -17,16 +17,12 @@ def ObtenerDatos():
     cjn_ciudades = list(range(1, largo_matriz))
     return (cjn_ciudades, filas_matriz)
 
-"""
 def Subconjuntos(conjunto):
-    total_sub = []
-    for i in range(0, len(conjunto)+1):
-        total_sub += list(itertools.combinations(conjunto, i))
-    return total_sub
+    """retorna todos los subconjuntos y sus rutas
 
-"""
-
-def Subconjuntos(conjunto):
+    return: list(rutas)
+    list rutas
+    """
     total_sub = []
     for i in range(0, len(conjunto)+1):
         if i == len(conjunto):
@@ -39,55 +35,72 @@ def Subconjuntos(conjunto):
                     total_sub.append([m,list(j)])
     return total_sub
 
-def CalcularRuta(data):
-    nodo_ir = data[0]
-    conj = data[1]
-    llave = str(nodo_ir)+"/"+str(conj)
-    if llave in valores_guardados:
-        costo = valores_guardados[llave]
-        print("hecho por diccionario")
-    else:
-        costo = matriz[nodo_inicial][nodo_ir]
+def Permutaciones(ruta):
+    nodo_final = ruta[0]
+    conjunto = ruta[1]
+    lista_costo = []
+    lista_nodos_ant = []
+    lista_rutas = []
+    if len(conjunto) <= 1:
+        #costo = CalcularCostoRuta(ruta)
+        #nodo_ant = 0
+        #if len(conjunto) == 1:
+        #    nodo_ant = conjunto[0]
+        costo, nodo_ant = CalcularCostoRuta(ruta)
         print(costo)
-        print("hecho por matriz")
-    valores_guardados[llave] = costo
-    print(valores_guardados)
-
-def CostoRuta(nodo_final, conjunto):
-    lista_ruta = []
-    if len(conjunto) == 1:
-        elemento = list(conjunto)[0]
-        print matriz[nodo_final][elemento]
-        return matriz[nodo_final][elemento]
+        Guardar(ruta,costo,nodo_ant)
     else:
         permutaciones_rutas = list(itertools.permutations(conjunto))
-        print(permutaciones)
+        #print(permutaciones_rutas) #[(),()]
         for ruta in permutaciones_rutas:
-            lista_ruta.append(CostoRuta(nodo_final,))
+            print("** "+str(ruta))
+            costo, nodo_ant = CalcularCostoRuta([nodo_final, list(ruta)])
+            lista_nodos_ant.append(nodo_ant)
+            lista_costo.append(costo)
+            lista_rutas.append(ruta)
+        minimo = min(lista_costo)
+        indice_minimo = lista_costo.index(minimo)
+        print("-"+str(lista_rutas[indice_minimo]))
+        Guardar(lista_rutas[indice_minimo], lista_costo[indice_minimo],lista_nodos_ant[indice_minimo])
 
-def Llave(nodo_final, lista_nodos):
-    return str(nodo_final)+"/"+str(lista_nodos)
+def Llave(ruta):
+    return str(ruta[0])+"/"+str(ruta[1])
 
-def CalcularCostoRuta(nodo_final, lista_nodos):
-    if len(lista_nodos) == 0:
-        llave_actual = Llave(nodo_final, lista_nodos)
-        costo = matriz[nodo_inicial][nodo_final]
-        valores_guardados[llave_actual] = costo
-        print(costo)
-        print(valores_guardados)
-        return
-    costo = matriz[lista_nodos[0]][nodo_final]
-    llave_actual = Llave(nodo_final, lista_nodos)
-    print(llave_actual)
-    llave_anterior = Llave(lista_nodos[0],lista_nodos[1:])
-    print(llave_anterior)
+def Guardar(ruta, costo,nodo_ant):
+    llave = Llave(ruta)
+    valores_guardados[llave] = (costo, nodo_ant)
+
+def Cargar(ruta):
+    llave_anterior = Llave(ruta)
     if llave_anterior in valores_guardados:
-        print("hay un dato guardado")
-        costo += valores_guardados[llave_anterior]
+        #print("hay un dato guardado")
+        return (True, valores_guardados[llave_anterior])
+        #valores_guardados[llave_actual] = costo
     else:
-        print("no hay dato guardado, procedo a calcular")
+        #print("no hay dato guardado de "+ llave_anterior+", procedo a calcular")
+        #print(valores_guardados)
         #CalcularCostoRuta(lista_nodos[0],lista_nodos[1:])
-    print(costo)
+        return (False, 0)
+
+
+def CalcularCostoRuta(ruta):
+    nodo_final = ruta[0]
+    lista_nodos = ruta[1]
+    if len(lista_nodos) == 0:
+        ##llave_actual = Llave(ruta)
+        costo = matriz[nodo_inicial][nodo_final]
+        ##valores_guardados[llave_actual] = costo
+        return (costo, nodo_inicial)
+    costo = matriz[lista_nodos[0]][nodo_final]
+    ruta_previa = [lista_nodos[0],lista_nodos[1:]]
+    estado_previo = Cargar(ruta_previa)
+    if estado_previo[0]:
+        data_guardada = estado_previo[1]
+        return (costo + data_guardada[0], lista_nodos[0])
+    else:
+        print("algo salio mal")
+        print(Llave(ruta_previa))
+        print(valores_guardados)
 
 nodo_inicial = 0
 matriz = [[0, 1, 15, 6], [2, 0, 7, 3], [9, 6, 0, 12],[10, 4, 8, 0]]
@@ -97,23 +110,28 @@ conjunto_ciudades = set([1,2,3])
 
 
 subconjuntos = Subconjuntos(conjunto_ciudades)
-print(subconjuntos)
-valores_guardados = {'2/()': 15, '1/[2]': 15}
-CalcularCostoRuta(2,[]) #3,[1,2]
+#print(subconjuntos)
+
+#Permutaciones(3,[1,2])
+
+valores_guardados = {'2/()': 15, '1/[2]': 15} # llave: (costo, nodo)
+valores_guardados = {}
+#CalcularCostoRuta([2,[]]) #3,[1,2]
 
 #print(subconjuntos)
 
 #CalcularRuta([2,()])
+
 """
-for i in subconjuntos:
-    print(len(i))
-    if len(i) == 1:
-        print(i[0])
-        print('')
-    elif len(i) == 2:
-        print(str(i[0])+' '+str(i[1]))
-        print('')
-    else:
-        print(i)
-        print('')
-        """
+for ruta in subconjuntos:
+    print("* "+str(Llave(ruta)))
+    CalcularCostoRuta(ruta)
+print(valores_guardados)
+"""
+
+for i in range(0,len(subconjuntos)):
+    if len(subconjuntos[i][1]) == 3:
+        break
+    print("* "+str(subconjuntos[i]))
+    Permutaciones(subconjuntos[i])
+print(valores_guardados)
